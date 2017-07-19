ea0f5143842e06163e51d4968be4b36490ebdb4a
kodaitakahashi <kodaitakahashi.kt@gmail.com>
2017-07-19 21:36:02 +0900

    fix

--- Q3/soul_number_normal.rb
+++ Q3/soul_number_normal.rb
@@ -5,11 +5,12 @@ ARGV[0].each_char do |ch|
   nums.push(ch.to_i)
 end
 added_num = 0
-nums.map do |num|
+nums.each do |num|
   added_num += num
 end
 
 str_nums = added_num.to_s.chars
+
 loop do
   result = str_nums[0].to_i + str_nums[1].to_i
   str_result = result.to_s
